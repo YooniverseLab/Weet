e721b40488a10ad928da9cc218fe0df25016440e
OutlookDev <pleasejumal@hotmail.com>
2026-04-09 17:36:46 +0900

    Fix stuck on Loading Screen

---
+++ lib/config/environment.dart
@@ -0,0 +1,5 @@
+class Environment {
+  static const String appwriteProjectId = '69d60c6d002470956bd1';
+  static const String appwriteProjectName = 'First App';
+  static const String appwritePublicEndpoint = 'https://sgp.cloud.appwrite.io/v1';
+}
